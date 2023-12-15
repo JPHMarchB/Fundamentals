c7acb6679f1cfe027f624a0c768569ddfcd8c600
Joseph Marchbanks <jmarchbanks2@cnm.edu>
2023-12-14 21:47:54 -0700

    Added conditionals-and-functions.js, objects-apis.js, and variables-and-data.js files.

---
+++ variables-and-data.js
@@ -0,0 +1,48 @@
+/*
+ *Variables
+ */
+let wordData;
+
+wordData = "Word list";
+
+console.log(wordData);
+
+let subTotal = 100;
+let tax = subTotal * 0.07;
+let totalWithTax = tax + subTotal;
+
+/*
+ *Data Types
+ */
+
+let obj = {
+    key1: "value",
+    key2: 2,
+    boolean: true,
+    obj2: {
+        obj2Key1: "Internal Object Value",
+    }
+};
+console.log(obj.obj2.obj2Key1);
+
+/*
+ *Typing
+ */
+
+let variable ="string";
+
+let var1 = "2";
+let var2 = 5;
+let result = var1 + var2;
+
+console.log(result)
+
+let add = 1+2;
+let sub = 2-1;
+let mult = 2*4
+let division = 4/2;
+let mod = 5 % 2;
+
+let string = "String 1 " + "String 2";
+
+console.log(string)
